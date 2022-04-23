928c3db1bceec0cf995b605d0443214e81a33d75
Joy <103285920+HepsibaJoicy@users.noreply.github.com>
2022-04-23 21:37:19 +0530

    Update script.js

--- script.js
+++ script.js
@@ -13,7 +13,7 @@ let songs = [
     {songName: "Hailee Stanfield - Capital letters", filePath: "songs/1.mp3", coverPath: "covers/1.jpg"},
     {songName: "The kid LORAI,Justin Bieber - STAY", filePath: "songs/2.mp3", coverPath: "covers/2.jpg"},
     {songName: "Anna Blue - Silent Scream", filePath: "songs/3.mp3", coverPath: "covers/3.jpg"},
-    {songName: "Different Heaven & EH!DE - My Heart [NCS Release]", filePath: "songs/4.mp3", coverPath: "covers/4.jpg"},
+    {songName: "The Chainsmokers,Illenium - Takeaway", filePath: "songs/4.mp3", coverPath: "covers/4.jpg"},
     {songName: "Janji-Heroes-Tonight-feat-Johnning-NCS-Release", filePath: "songs/5.mp3", coverPath: "covers/5.jpg"},
     {songName: "Rabba - Salam-e-Ishq", filePath: "songs/2.mp3", coverPath: "covers/6.jpg"},
     {songName: "Sakhiyaan - Salam-e-Ishq", filePath: "songs/2.mp3", coverPath: "covers/7.jpg"},
